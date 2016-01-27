5838cf8c89cd49940a45ab613c12c367e3cb7b74
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 07:42:16 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -1,26 +1,36 @@
 var coorde = $("body").find(".tactile-timemachine__preview-background-thumbnail").attr("src");
 
-var c = coorde.indexOf('panoid='); 
-var L = coorde.indexOf('&w=');
+var c0 = coorde.indexOf('panoid=');
+var c=coorde.indexOf('panoid=',c0-7);
+var L0 = coorde.indexOf('&w=');
+var L = coorde.indexOf('&w=',L0-3);
 var pano = coorde.slice(c,L);
 
-var c2 = coorde.indexOf('pitch='); 
-var L2 = coorde.indexOf('&ll');
+var c20 = coorde.indexOf('pitch=');
+var c2=coorde.indexOf('pitch=',c20-6);
+var L20 = coorde.indexOf('&ll');
+var L2=coorde.indexOf('&ll',L20-3);
 var pitch = coorde.slice(c2,L2);
 
-var c3 = coorde.indexOf('&yaw='); 
-var L3 = coorde.indexOf('&pitch');
+var c30 = coorde.indexOf('&yaw=');
+var c3=coorde.indexOf('&yaw=',c30-5);
+var L30 = coorde.indexOf('&pitch');
+var L3=coorde.indexOf('&pitch',L30-6);
 var heading = coorde.slice(c3,L3);
 
 
 var coorde2 = window.location.href;
 
-var c4 = coorde2.indexOf('a,'); 
-var L4 = coorde2.indexOf('y,');
+var c40 = coorde2.indexOf('a,');
+var c4=coorde2.indexOf('a,',c40-2);
+var L40 = coorde2.indexOf('y,');
+var L4=coorde2.indexOf('y,',L40-2);
 var fov = coorde2.slice(c4,L4);
 
-var c5 = coorde2.indexOf('/@'); 
-var L5 = coorde2.indexOf(',3a');
+var c50 = coorde2.indexOf('/@');
+var c5=coorde2.indexOf('/@',c50-2);
+var L50 = coorde2.indexOf(',3a');
+var L5=coorde2.indexOf(',3a',L50-3);
 var ll = coorde2.slice(c5,L5);
 
 console.log(pano,pitch,heading,fov,ll);
