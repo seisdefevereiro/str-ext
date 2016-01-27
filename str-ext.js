2f917e12a0dfa5d2e5e8cdc963eb86c38b792826
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 08:59:52 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -10,26 +10,20 @@ var c2 = coorde.indexOf('pitch=');
 var L2 = coorde.indexOf('&ll');
 var pitch = coorde.substring(c2+6,L2);
 
-var c30 = coorde.indexOf('&yaw=');
-var c3=coorde.indexOf('&yaw=',c30-5);
-var L30 = coorde.indexOf('&pitch');
-var L3=coorde.indexOf('&pitch',L30-6);
-var heading = coorde.slice(c3,L3);
+var c3 = coorde.indexOf('&yaw=');
+var L3 = coorde.indexOf('&pitch');
+var heading = coorde.substring(c3+5,L3);
 
 
 var coorde2 = window.location.href;
 
-var c40 = coorde2.indexOf('a,');
-var c4=coorde2.indexOf('a,',c40-2);
-var L40 = coorde2.indexOf('y,');
-var L4=coorde2.indexOf('y,',L40-2);
-var fov = coorde2.slice(c4,L4);
+var c4 = coorde2.indexOf('a,');
+var L4 = coorde2.indexOf('y,');
+var fov = coorde2.substring(c4+2,L4);
 
-var c50 = coorde2.indexOf('/@');
-var c5=coorde2.indexOf('/@',c50-2);
-var L50 = coorde2.indexOf(',3a');
-var L5=coorde2.indexOf(',3a',L50-3);
-var ll = coorde2.slice(c5,L5);
+var c5 = coorde2.indexOf('/@');
+var L5 = coorde2.indexOf(',3a');
+var ll = coorde2.substring(c5+2,L5);
 
 
 console.log(pano,pitch,heading,fov,ll);
