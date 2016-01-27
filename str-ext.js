37cd805cffb16ca5c9a844b66229536b78dca28a
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 09:19:39 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -28,8 +28,8 @@ var ll = coorde2.substring(c5+2,L5);
 
 console.log(pano,pitch,heading,fov,ll);
 
-document.body.innerHTML += "<div id='popup' style='background:white;width:500px;height:800px;position:fixed'></div>";
-document.getElementById("popup").innerHTML = "<img id='rosa' src='//maps.googleapis.com/maps/api/streetview?size=300x300&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/>";
+document.body.innerHTML += "<div id='popup' style='background:white;width:500px;height:800px;position:fixed;right:0'></div>";
+document.getElementById("popup").innerHTML = "<img id='rosa' src='//maps.googleapis.com/maps/api/streetview?size=500x500&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/>";
 
 imagem.onchange = (function() { 
   var imagem = document.getElementsByClassName("tactile-timemachine__preview-background-thumbnail")[0];
