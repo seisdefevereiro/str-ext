9ee702fd79fe34149ed9a67ab230ceac1900e2d5
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 06:57:12 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -1,1 +1,16 @@
 console.log(window.location.href);
+
+var coorde = $("body").find(".tactile-timemachine__preview-background-thumbnail").attr("src");
+var c = coorde.indexOf('panoid='); 
+var L = coorde.indexOf('&w=');
+var pano = coorde.slice(c,L);
+
+var c2 = coorde.indexOf('pitch='); 
+var L2 = coorde.indexOf('&ll');
+var pitch = coorde.slice(c2,L2);
+
+console.log(pano,pitch);
+
+
+$("<div id='popup' style="background:white;width:500;height:800;position:fixed"><div/>").appendTo("body");
+$("#popup").append("<img src='http://maps.googleapis.com/maps/api/streetview?size=300x300&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/>")
