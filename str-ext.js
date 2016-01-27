0318b8e854dc681dee41b1af19d32888efb99f7f
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 13:40:10 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -12,6 +12,35 @@ new MutationObserver(function (mutations) {
                 });
                 if (!found) {
                     versions.push(name);
+                    
+                    
+var coorde = name;
+
+var c = coorde.indexOf('panoid=');
+var L = coorde.indexOf('&w=');
+var pano = coorde.substring(c+7,L);
+
+var c2 = coorde.indexOf('pitch=');
+var L2 = coorde.indexOf('&ll');
+var pitch = coorde.substring(c2+6,L2);
+
+var c3 = coorde.indexOf('&yaw=');
+var L3 = coorde.indexOf('&pitch');
+var heading = coorde.substring(c3+5,L3);
+
+
+var coorde2 = window.location.href;
+
+var c4 = coorde2.indexOf('a,');
+var L4 = coorde2.indexOf('y,');
+var fov = coorde2.substring(c4+2,L4);
+
+var c5 = coorde2.indexOf('/@');
+var L5 = coorde2.indexOf(',3a');
+var ll = coorde2.substring(c5+2,L5);
+                    
+document.getElementById("popup").innerHTML += "<img class='rosa' src='//maps.googleapis.com/maps/api/streetview?size=500x500&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/><br>";
+
                     console.log(versions);
                 }
             }
@@ -32,4 +61,5 @@ new MutationObserver(function (mutations) {
 });
 
 var amen = document.getElementsByClassName("widget-titlecard")[0];
-amen.innerHTML += "<div id='popup' style='background:white;width:10px;height:10px;position:absolute;'></div>";
+amen.innerHTML += "<div id='popup' style='background:white;width:400px;height:800px;position:fixed;top:15px;right:15px'></div>";
+
