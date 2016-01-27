60dcbcb51ac709041ef457776f14502e82fa973a
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-27 11:34:04 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -1,36 +1,21 @@
 var versions = [];
-
-
 var imagem = document.getElementsByClassName("tactile-timemachine__preview-background-thumbnail")[0];
 var coorde = imagem.src;
 versions.push(coorde);
 
-
 new MutationObserver(function (mutations) {
     mutations.some(function(mutation) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-            console.log(mutation);
-            console.log('Old src: ', mutation.oldValue);
-            
-            
-            
-
-
-function add(name) {
-    var found = versions.some(function (el) {
-      return el === name;
-    });
-    if (!found) {
-        versions.push(name);
-    }
-}
-
-add(mutation.target.src);
-
-
-
-            console.log('New src: ', mutation.target.src);
-            console.log(versions);
+            function add(name) {
+                var found = versions.some(function (el) {
+                    return el === name;
+                });
+                if (!found) {
+                    versions.push(name);
+                    console.log(versions);
+                }
+            }
+            add(mutation.target.src);
             return true;
         }
 
@@ -46,5 +31,3 @@ add(mutation.target.src);
     subtree: true
 });
 
-
-console.log(versions)
