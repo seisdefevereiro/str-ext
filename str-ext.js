0d800723a2b5b32fd521767ba7e28ca015dac0cd
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-28 23:34:01 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -32,7 +32,7 @@ var ll = coorde2.substring(c5+2,L5);
 document.getElementById("popup").innerHTML += "<img class='rosa' style='margin:70px' src='//maps.googleapis.com/maps/api/streetview?size=450x250&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/><br>";
 }
 
-loadimages();
+
 
 new MutationObserver(function (mutations) {
     mutations.some(function(mutation) {
@@ -68,6 +68,8 @@ loadimages();
 
 var amen = document.getElementsByClassName("widget-titlecard")[0];
 amen.innerHTML += "<div id='popup' style='background:white;text-align:center;overflow:auto;width:1100px;height:850px;position:fixed;top:15px;right:15px'></div>";
+
+loadimages();
 
 var dragPoints = [];
 var i = 1;
