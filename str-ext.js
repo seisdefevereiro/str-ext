db74e3852016471694ae6479e9fb3ec29df5f8de
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-30 09:43:48 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -40,17 +40,11 @@ var L5 = coorde2.indexOf(',3a');
 var ll = coorde2.substring(c5+2,L5);
 
 
-    (function () {
-var keytrana = 'cravo'+nenem;
-
 document.getElementById("popup").innerHTML += 
-"<div id='"+keytrana+"' style='margin:70px;display:inline-block'><img class='rosa' style='' src='//maps.googleapis.com/maps/api/streetview?size=450x640&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/><form>fov:<span><input type='button' value='-'><input type='text' style='width:20px;background:yellow' name='name' value='"+fov+"' /><input type='button' value='+'></span></form></div>";
+"<div id='cravo"+nenem+"' style='margin:70px;display:inline-block'><img class='rosa' style='' src='//maps.googleapis.com/maps/api/streetview?size=450x640&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/><form>fov:<span><input type='button' value='-'><input type='text' style='width:20px;background:yellow' name='name' value='"+fov+"' /><input type='button' value='+'></span></form></div>";
 nenem++
-var keytrana2 = document.getElementById(keytrana);
+var keytrana2 = document.getElementById('cravo'+nenem);
 keytrana2.getElementsByTagName("input")[2].addEventListener("click", function() {makeith(keytrana2)});
-    }())
-    
-
 
 
 }
