a6544dcc6299c542a72e5f12a4c6f96291e65323
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-30 11:35:36 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -84,7 +84,7 @@ loadimages();
 });
 
 var amen = document.getElementsByClassName("widget-titlecard")[0];
-amen.innerHTML += "<div id='mirror' style='z-index:99999;top:0;right:0;left:0;width:100%;height:100%;position:fixed;box-sizing: border-box;padding: 16px'><div id='popup' style='background:white;text-align:center;overflow:auto;width:calc(100% - 8px);height:calc(100% - 8px)'><p id='dispara'></div></div></div>";
+amen.innerHTML += "<div id='mirror' style='z-index:99999;top:0;right:0;left:0;width:100%;height:100%;position:fixed;box-sizing: border-box;padding: 16px'><div id='popup' style='background:white;text-align:center;overflow:auto;width:calc(100% - 8px);height:calc(100% - 8px)'><p id='dispara'>dispara</div></div></div>";
 
 
 loadimages();
