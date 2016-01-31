1bb739edfb274afc06024808210a892d537ee57d
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-31 07:16:24 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -86,13 +86,15 @@ loadimages();
 var amen = document.getElementsByClassName("widget-titlecard")[0];
 amen.innerHTML += "<div id='mirror' style='z-index:99999;top:0;right:0;left:0;width:100%;height:100%;position:fixed;box-sizing: border-box;padding: 16px'><div id='popup' style='background:white;text-align:center;overflow:auto;width:calc(100% - 8px);height:calc(100% - 8px)'><span id='dispara'>dispara</span></div></div>";
 
-
-var prepareButton = function() {
-document.getElementById("dispara").onclick = function(){
+var mir = document.getElementById("popup");
+var iDiva = mir.createElement('div');
+iDiva.onload = function(){
+            console.log("uuuuu");
+};
+iDiva.onclick = function(){
             console.log("loo");
 };
-}
-prepareButton();
+
 
 loadimages();
 
