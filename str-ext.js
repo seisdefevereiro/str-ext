5b1919a0ac33b38b0c661616e6f86e9de60dab2b
seisdefevereiro <seisdefevereiro@gmail.com>
2016-02-01 03:09:45 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -83,7 +83,7 @@ new MutationObserver(function (mutations) {
 });
 
 var amen = document.getElementsByClassName("widget-titlecard")[0];
-amen.innerHTML += "<div id='mirror' style='z-index:99999;top:0;right:0;left:0;width:100%;height:100%;position:fixed;box-sizing: border-box;padding: 16px'><div id='popup' style='background:white;text-align:center;overflow:auto;width:calc(100% - 8px);height:calc(100% - 8px)'><span id='dispara'>dispara</span></div></div>";
+amen.innerHTML += "<div id='mirror' style='z-index:99999;top:0;right:0;left:0;width:100%;height:100%;position:fixed;box-sizing: border-box;padding: 16px'><div id='popup' style='background:white;text-align:center;overflow:auto;width:calc(100% - 8px);height:calc(100% - 8px)'><span id='dispara'>dispara</span><canvas id='tela' style='border:3px solid yellow;position:fixed;'></canvas></div></div>";
 
 
 amen.onclick = function(event) {
@@ -105,6 +105,96 @@ amen.onclick = function(event) {
                 }())
         }
     }
+    
+var w = document.getElementById("popup").innerWidth;
+var h = document.getElementById("popup").innerHeight;
+       var canvas = document.getElementById("tela"),
+            ctx = canvas.getContext("2d"),
+            painting = false,
+            lastX = 0,
+            lastY = 0,
+            lineThickness = 40;
+       canvas.style.width = w; canvas.style.height = h;
+        canvas.onmousedown = function(e) {
+            painting = true;
+            ctx.fillStyle = "#000";
+            lastX = e.pageX - this.offsetLeft;
+            lastY = e.pageY - this.offsetTop;
+        };
+
+        canvas.onmouseup = function(e){
+            painting = false;
+        }
+
+        canvas.onmousemove = function(e) {
+            if (painting) {
+                mouseX = e.pageX - this.offsetLeft;
+                mouseY = e.pageY - this.offsetTop;
+
+                // find all points between        
+                var x1 = mouseX,
+                    x2 = lastX,
+                    y1 = mouseY,
+                    y2 = lastY;
+
+
+                var steep = (Math.abs(y2 - y1) > Math.abs(x2 - x1));
+                if (steep){
+                    var x = x1;
+                    x1 = y1;
+                    y1 = x;
+
+                    var y = y2;
+                    y2 = x2;
+                    x2 = y;
+                }
+                if (x1 > x2) {
+                    var x = x1;
+                    x1 = x2;
+                    x2 = x;
+
+                    var y = y1;
+                    y1 = y2;
+                    y2 = y;
+                }
+
+                var dx = x2 - x1,
+                    dy = Math.abs(y2 - y1),
+                    error = 0,
+                    de = dy / dx,
+                    yStep = -1,
+                    y = y1;
+
+                if (y1 < y2) {
+                    yStep = 1;
+                }
+
+                lineThickness = 10 - Math.sqrt((x2 - x1) *(x2-x1) + (y2 - y1) * (y2-y1))/10;
+                if(lineThickness < 1){
+                    lineThickness = 1;   
+                }
+
+                for (var x = x1; x < x2; x++) {
+                    if (steep) {
+                        ctx.fillRect(y, x, lineThickness , lineThickness );
+                    } else {
+                        ctx.fillRect(x, y, lineThickness , lineThickness );
+                    }
+
+                    error += de;
+                    if (error >= 0.5) {
+                        y += yStep;
+                        error -= 1.0;
+                    }
+                }
+
+                lastX = mouseX;
+                lastY = mouseY;
+
+            }
+        }
+    
+    
 }
 
 function makeItHappen(inp,elem){
@@ -168,5 +258,7 @@ var i = 1;
 var element = document.getElementsByClassName("tactile-timemachine__scrubber")[0];
 dispatchHTMLMouseEvent("mousedown", dragPoints[0], element);
 sendMouseDrag(element, dragPoints);
+
+
 
 
