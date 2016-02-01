c130ded1f09f304ae444704db9937a4cf4842d2c
seisdefevereiro <seisdefevereiro@gmail.com>
2016-01-31 21:08:54 -0300

    Update str-ext.js

--- str-ext.js
+++ str-ext.js
@@ -42,7 +42,7 @@ var c5 = coorde2.indexOf('/@');
 var L5 = coorde2.indexOf(',3a');
 var ll = coorde2.substring(c5+2,L5);
 
-console.log(diad);
+
 document.getElementById("popup").innerHTML += "<div id='cravo"+nenem+"' style='margin:70px;display:inline-block'><img class='rosa' style='' src='//maps.googleapis.com/maps/api/streetview?size=450x640&location="+ll+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&pano="+pano+"'/><span>"+diad+"</span><form>fov:<span><input type='button' value='-' class='minus'><input type='text' style='width:30px;background:yellow' name='name' value='"+fov+"' /><input type='button' class='plus' value='+'></span></form></div>";
 
 
@@ -52,7 +52,7 @@ nenem++
 
 
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  var list = document.querySelector('div.tactile-timemachine__date span span');
+  var list = document.querySelector('div.tactile-timemachine__date span span span');
   
   var observer = new MutationObserver(function(mutations) {  
     mutations.forEach(function(mutation) {
